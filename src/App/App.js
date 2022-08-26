c018e08a2e0f997ec09a8ed5fe3014a45c5c052e
kds <kdskoudous98@gmail.com>
2022-08-26 14:16:03 +0100

    delete Products component

--- src/App/App.js
+++ src/App/App.js
@@ -11,8 +11,8 @@ function App() {
   return (
     <div className="App">
       <Header/>
-      {/* <Home/> */}
-      <Market/>
+      <Home/>
+      {/* <Market/> */}
       <Footer/>
     </div>
   );
